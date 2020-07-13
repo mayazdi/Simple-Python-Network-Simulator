8ef06d6b9b3d1807b52b8df661cb2542b8f78fa6
Amin <yazdi.mohammad99@gmail.com>
2020-07-13 18:20:12 +0430

    Unweighted Bellmanford

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ class Graph:
 		self.V = vertices
 		self.graph = []
 
-	def addEdge(self, u, v, w):
-		self.graph.append([u, v, w])
+	def addEdge(self, u, v):
+		self.graph.append([u, v])
+		self.graph.append([v, u])
 
 	def printArr(self, dist):
 		print("Vertex Distance from Source")
@@ -19,14 +20,14 @@ class Graph:
 
 		for _ in range(self.V - 1):
 
-			for u, v, w in self.graph:
-				if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-						dist[v] = dist[u] + w
+			for u, v in self.graph:
+				if dist[u] != float("Inf") and dist[u] + 1 < dist[v]:
+						dist[v] = dist[u] + 1
 
-		for u, v, w in self.graph:
-				if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-						print("Graph contains negative weight cycle")
-						return
+		# for u, v in self.graph:
+		# 		if dist[u] != float("Inf") and dist[u] + 1 < dist[v]:
+		# 				print("Graph contains negative weight cycle")
+		# 				return
 		
 		self.printArr(dist)
 
@@ -42,13 +43,10 @@ for i in range(0, int(ln)):
     li.append(input())
 
 g = Graph(5)
-g.addEdge(0, 1, -1)
-g.addEdge(0, 2, 4)
-g.addEdge(1, 2, 3)
-g.addEdge(1, 3, 2)
-g.addEdge(1, 4, 2)
-g.addEdge(3, 2, 5)
-g.addEdge(3, 1, 1)
-g.addEdge(4, 3, -3)
+g.addEdge(0, 1)
+g.addEdge(2, 1)
+g.addEdge(3, 1)
+g.addEdge(3, 4)
+g.addEdge(2, 3)
 
 g.BellmanFord(0)
