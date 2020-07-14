cea8eb3c3182303c4fb1504a0d946d0d1096cb50
Amin <yazdi.mohammad99@gmail.com>
2020-07-15 01:31:35 +0430

    Move functions to class

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ class Graph:
 
 	def __init__(self, vertices):
 		self.V = vertices
+		self.nodes_dic = {}
+		self.netwk_dic = {}
 		self.graph = []
+	
+	def add_node_to_dic(self, val, i):
+		self.nodes_dic[val] = i
 
-	def addEdge(self, u, v):
-		self.graph.append([u, v])
-		self.graph.append([v, u])
+	def get_nodes_dic(self):
+		return self.nodes_dic
+
+	def get_netwk_dic(self):
+		return self.netwk_dic
+
+	def addNetwork(self, net_addr, nd1, nd2):
+		self.netwk_dic[net_addr] = (nd1, nd2)
+	
+	def addEdge(self, nd1, nd2):
+		self.graph.append([self.nodes_dic[nd1], self.nodes_dic[nd2]])
+		# self.graph.append([u, v])
+		# self.graph.append([v, u])
 
 	def printArr(self, dist):
 		print("Vertex Distance from Source")
@@ -26,9 +41,11 @@ class Graph:
 		# for u, v in self.graph:
 		# 		if dist[u] != float("Inf") and dist[u] + 1 < dist[v]:
 		# 				print("Graph contains negative weight cycle")
-		# 				return
-		
+		# 				return		
 		self.printArr(dist)
+
+
+# def print_distances():
 
 
 def print_seperator():
@@ -38,10 +55,8 @@ def print_seperator():
 
 print("Enter the graph Size:")
 ln = input()
-li = []
+g = Graph(int(ln))
 nodes = []
-nodes_dic = {}
-netwk_dic = {}
 
 print("Deafult Names(R0, R1, ..., R(n-1))?[Y/n]")
 default_names = input()
@@ -49,27 +64,33 @@ if default_names=="Y" or default_names=="Yes" or default_names=="":
 	for i in range(int(ln)):
 		val = "R" + str(i)
 		nodes.append(val)
-		nodes_dic[val] = i
+		g.add_node_to_dic(val, i)
 else:
 	print("Enter the values then:")
 	for i in range(int(ln)):
 		print(str(i) + ":", end="")
 		val = input()
 		nodes.append(val)
-		nodes_dic[val] = i
+		g.add_node_to_dic(val, i)
 
 print("names: " + str(nodes))
-print(nodes_dic)
+print(g.get_nodes_dic())
 print_seperator()
 print("Enter the graph topology in the following format:\nNode1 Node2 (XXX.XXX.XXX.XXX)\n")
 for i in range(0, int(ln)):
     print("{count}:".format(count=i+1), end="")
     edge = input()
     nd1, nd2, net_addr = edge.split(' ')
-    netwk_dic[net_addr] = (nd1, nd2)
+    # g.addEdge(nodes_dic[nd1], nodes_dic[nd2])
+    g.addEdge(nd1, nd2)
+    g.addNetwork(net_addr, nd1, nd2)
+    # netwk_dic[net_addr] = (nd1, nd2)
 
 print_seperator()
+
 #do all the routing things
+
+""" 
 g = Graph(5)
 g.addEdge(0, 1)
 g.addEdge(2, 1)
@@ -78,13 +99,14 @@ g.addEdge(3, 4)
 g.addEdge(2, 3)
 
 g.BellmanFord(0)
-
-print_seperator()
+"""
 i = 0
-netwk_dic.keys()
-for item in netwk_dic.keys():
+NK = g.get_netwk_dic().keys()
+for item in NK:
 	print(str(i)+":"+item)
 	i+=1
 print("Pick a Network from above.\n>>", end="")
 net_selected=input()
-print(netwk_dic[net_selected])
+# print(netwk_dic[net_selected])
+# print_distances()
+# print(min(g.BellmanFord(nodes_dic[netwk_dic[net_selected][0]]),g.BellmanFord(nodes_dic[netwk_dic[net_selected][1]])))
